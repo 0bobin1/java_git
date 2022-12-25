8f8588fae18392bc62a59b43acb74cd995246137
lubb <2820843645@qq.com>
2022-12-25 20:13:47 +0800

    study04

--- study04/src/number/Demo02.java
+++ study04/src/number/Demo02.java
@@ -4,9 +4,9 @@ import static java.lang.Math.abs;
 
 public class Demo02 {
     public static void main(String[] args) {
-        float a=2.5f;
-        double b=3.606626262626;
-        System.out.println(Math.abs(a-(float)b));
+        float a = 2.5f;
+        double b = 3.606626262626;
+        System.out.println(Math.abs(a - (float) b));
         System.out.println(abs(-2.3));
     }
 }
