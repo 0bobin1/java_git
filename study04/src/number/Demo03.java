cfee479f44f62311ea43d5fbd191aecd06339115
lubb <2820843645@qq.com>
2022-12-25 21:16:08 +0800

    study04

--- study04/src/number/Demo03.java
+++ study04/src/number/Demo03.java
@@ -2,6 +2,10 @@ package number;
 
 public class Demo03 {
     public static void main(String[] args) {
-
+        double a = -1.5;
+        System.out.println(Math.floor(a));//向下取整
+        System.out.println(Math.round(a));//四舍五入   (x+0.5)再向下取整
+        System.out.println(Math.ceil(a));//向上取整
+        System.out.println("66\t65\t55");
     }
 }
